fcc3f8fb8e59997d9205c4ea44f5664857e3abda
Yitzchok Kimelman <itchkimel@gmail.com>
2021-06-03 23:56:30 -0400

    added filtered

--- src/components/GuitarsContainer.js
+++ src/components/GuitarsContainer.js
@@ -10,7 +10,7 @@ import Form from 'react-bootstrap/Form'
 export default class GuitarsContainer extends Component{
 
   state = {
-    showHide: false
+    showHide: false,
   }
 
   showComponent = () => {
@@ -19,121 +19,101 @@ export default class GuitarsContainer extends Component{
 
   render(){
 
-      return(
-        <div>
-          <Jumbotron className="text-center">
-              <h1 className="nav-text">The Guitars Collection</h1>
-              <p>
-                Dive into our handpicked selection of axes, all guaranteed to pack some heat into your rig.
-              </p>
-              <p>
-                <Button variant="outline-dark">Search by Filter</Button>
-              </p>
-              <p>
-              <Form className="form_content_wrap">
-                  <Form.Group>
-                    <Form.Label>Brand: &nbsp;</Form.Label>
+    return(
+      <div>
+        <Jumbotron className="text-center">
+            <h1 className="nav-text">The Guitars Collection</h1>
+            <p>
+              Dive into our handpicked selection of axes, all guaranteed to pack some heat into your rig.
+            </p>
+            <p>
+              <Button variant="outline-dark">Search by Filter</Button>
+            </p>
+            <p>
+            <Form className="form_content_wrap">
+                <Form.Group>
+                  <Form.Label>Brand: &nbsp;</Form.Label>
 
-                    <Form.Check
-                      inline
-                      label="Gibson"
-                      name="Gibson"
-                      />
-                      <Form.Check
-                      inline
-                      label="Fender"
-                      name="Fender"
-                      />
-                      <Form.Check
-                      inline
-                      label="Gretsch"
-                      name="Gretsch"
-                      />
-                      <Form.Check
-                      inline
-                      label="Danelectro"
-                      name="Danelectro"
-                      />
-                      <Form.Check
-                      inline
-                      label="Charvel"
-                      name="Charvel"
-                      />
-                      <Form.Check
-                      inline
-                      label="ESP"
-                      name="ESP"
-                      />
-                      <Form.Check
-                      inline
-                      label="ESP"
-                      name="ESP"
-                      />
-                      <Form.Check
-                      inline
-                      label="Ibanez"
-                      name="Ibanez"
-                      />
-                      <Form.Check
-                      inline
-                      label="Jackson"
-                      name="Jackson"
-                      />
-                      <Form.Check
-                      inline
-                      label="Rickenbacker"
-                      name="Rickenbacker"
-                      />
-                  </Form.Group>
-
-                  <Form.Group>
-                    {/* <Form.Label>Model: &nbsp;</Form.Label>
-
-                    <Form.Check
-                      inline
-                      label="Les Paul"
-                      name="Les Paul"
-                      // onChange={(e) => onRadioChange(e)}
-                    /> */}
-                  </Form.Group>
-
-                  <Form.Group>
-                    <Form.Label>Keyword</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="search description"
-                      // value={descValue}
-                      // onChange={(e) => setDescValue(e.currentTarget.value)}
+                  <Form.Check
+                    inline
+                    label="Gibson"
+                    name="Gibson"
                     />
-                  </Form.Group>
+                    <Form.Check
+                    inline
+                    label="Fender"
+                    name="Fender"
+                    />
+                    <Form.Check
+                    inline
+                    label="Gretsch"
+                    name="Gretsch"
+                    />
+                    <Form.Check
+                    inline
+                    label="Danelectro"
+                    name="Danelectro"
+                    />
+                    <Form.Check
+                    inline
+                    label="Charvel"
+                    name="Charvel"
+                    />
+                    <Form.Check
+                    inline
+                    label="ESP"
+                    name="ESP"
+                    />
+                    <Form.Check
+                    inline
+                    label="ESP"
+                    name="ESP"
+                    />
+                    <Form.Check
+                    inline
+                    label="Ibanez"
+                    name="Ibanez"
+                    />
+                    <Form.Check
+                    inline
+                    label="Jackson"
+                    name="Jackson"
+                    />
+                    <Form.Check
+                    inline
+                    label="Rickenbacker"
+                    name="Rickenbacker"
+                    />
+                </Form.Group>
 
+                <Form.Group >
+                  <Form.Label>Keyword</Form.Label>
+                  <Form.Control onChange={(e) => this.props.handleFilter(e)}
+                    type="text"
+                    placeholder="search description"/>
+                </Form.Group>
 
-                  <Button
-                    variant="primary"
-                    type="submit"
-                    // onClick={onFormSubmit}
-                    style={{ marginRight: "10px" }}
-                  >
-                    Submit
-                  </Button>
-                  <Button variant="primary" type="submit">
-                    Clear
-                  </Button>
-               
-              </Form>
-              </p>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  style={{ marginRight: "10px" }}
+                >
+                  Submit
+                </Button>
+                <Button variant="primary" type="submit">
+                  Clear
+                </Button>
+              
+            </Form>
+            </p>
+          </Jumbotron>
 
-            </Jumbotron>
-
-
-            <Container>
-                <Row lg={4}>
-                    {this.props.guitars.map(guitar => <GuitarCard key={guitar.id} guitar={guitar} {...this.props.routerProps} addToCart={this.props.addToCart} loggedIn={this.props.loggedIn} />)}
-                </Row>
-            </Container>
-        </div>    
-      )
+          <Container>
+              <Row lg={4}>
+                  {this.props.guitars.map(guitar => <GuitarCard key={guitar.id} guitar={guitar} {...this.props.routerProps} addToCart={this.props.addToCart} loggedIn={this.props.loggedIn} />)}
+              </Row>
+          </Container>
+      </div>    
+    )
   }
 }
-
-// {this.props.guitars.map(guitar => <GuitarCard guitar={guitar}/>)}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-
-
 export default class Navigation extends Component{
-
-
-
 
   render(){
  
@@ -17,8 +12,6 @@ export default class Navigation extends Component{
               <Navbar.Brand as={Link} to="/" className="nav-text">Nick's Guitar Bar</Navbar.Brand>
               <Nav className="mr-auto">
               <Nav.Link as={Link} to="/guitars">Guitars</Nav.Link>
-              {/* {<Nav.Link as={Link} to="/yourguitars">Your Collection</Nav.Link>} */}
-              {/* <Nav.Link onclick={<LoginModal></LoginModal>}> Login </Nav.Link> */}
               {this.props.loggedIn === false ? null : <Nav.Link as={Link} to="/yourguitars">Your Collection</Nav.Link>}
               {this.props.loggedIn === false ? null : <Nav.Link as={Link} to="/cart"> Cart </Nav.Link>}
             </Nav>
